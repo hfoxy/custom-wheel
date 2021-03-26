d9371b59290170fef8ccbdec1bc50ff6f708f01a
Adrian Clarke <33432074+VoleVoel@users.noreply.github.com>
2021-03-26 15:13:35 +0000

    Removed Gpio stuff, Made new Matrix files.

---
+++ Matrix.c
@@ -0,0 +1,22 @@
+//
+// Created by p209287 on 26/03/2021.
+//
+
+#include "pico/stdlib.h"
+#include "Matrix.h"
+
+#define COL_0 1
+#define COL_1 2
+#define COL_2 3
+#define COL_3 4
+
+#define ROW_0 5
+#define ROW_1 6
+#define ROW_2 7
+#define ROW_3 8
+
+
+void Matrix_Initialise(void)
+{
+
+}
---
+++ Matrix.h
@@ -0,0 +1,10 @@
+//
+// Created by p209287 on 26/03/2021.
+//
+
+#ifndef CUSTOM_WHEEL_MATRIX_H
+#define CUSTOM_WHEEL_MATRIX_H
+
+
+
+#endif //CUSTOM_WHEEL_MATRIX_H

7716b916aa7d475f89655fdc4ded1254ed400e0f
p209287 <p2092877@gmail.com>
2024-09-22 14:36:14 +0100

    Modified function declarations for SDK update.

--- fox-wheel.c
+++ fox-wheel.c
@@ -6,7 +6,7 @@
 #include "pico/multicore.h"
 
 // #include "bsp/board.h"
-// #include "tusb.h"
+ #include "tusb.h"
 
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
@@ -547,7 +547,7 @@ void tud_resume_cb(void) {
 // Invoked when received GET_REPORT control request
 // Application must fill buffer report's content and return its length.
 // Return zero will cause the stack to STALL request
-uint16_t tud_hid_get_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen) {
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen) {
     // TODO not Implemented
     (void) report_id;
     (void) report_type;
@@ -558,7 +558,7 @@ uint16_t tud_hid_get_report_cb(uint8_t report_id, hid_report_type_t report_type,
 
 // Invoked when received SET_REPORT control request or
 // received data on OUT endpoint ( Report ID = 0, Type = 0 )
-void tud_hid_set_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize) {
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize) {
     // This example doesn't use multiple report and report ID
     (void) report_id;
     (void) report_type;
